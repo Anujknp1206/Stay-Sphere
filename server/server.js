2e0c24796944a461666611e1551be04c3c65d256
Anuj YAdav <anujknp1206@gmail.com>
2024-12-10 09:43:49 +0530

    prepared app for production

--- server/server.js
+++ server/server.js
@@ -23,15 +23,15 @@ app.use("/api/bookings", bookingRoutes);
 app.use("/api/users", userRoutes);
 
 // setup production
-// if (process.env.NODE_ENV === "production") {
-//   const publicpath = path.join(__dirname, ".", "build");
-//   const filePath = path.resolve(__dirname, ".", "build", "index.html");
-//   app.use(express.static(publicpath));
+if (process.env.NODE_ENV === "production") {
+  const publicpath = path.join(__dirname, ".", "build");
+  const filePath = path.resolve(__dirname, ".", "build", "index.html");
+  app.use(express.static(publicpath));
 
-//   app.get("*", (req, res) => {
-//     return res.sendFile(filePath);
-//   });
-// }
+  app.get("*", (req, res) => {
+    return res.sendFile(filePath);
+  });
+}
 
 app.use(errorHandler);
 
